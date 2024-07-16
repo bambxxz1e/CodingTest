b7affb3d6fcf7b765c0737983211a68dbd725674
yunseongyeon <165138530+bambxxz1e@users.noreply.github.com>
2024-07-16 09:23:38 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int price) {
         int a = (int)(price / 100000);
         int answer = 0;
-        if(a >= 5) answer = (int)(price - (price * 0.2));
-        else if(a < 5 && a >= 3) answer = (int)(price - (price * 0.1));
-        else if(a < 3 && a >= 1) answer = (int)(price - (price * 0.05));
+        if(a >= 5) answer = (int)(price * 0.8);
+        else if(a < 5 && a >= 3) answer = (int)(price * 0.9);
+        else if(a < 3 && a >= 1) answer = (int)(price * 0.95);
         else answer = price;
         return answer;
     }
