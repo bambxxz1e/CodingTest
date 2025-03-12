da36329a1cea128c8b07017c3bcbf779e8ba7f24
yunseongyeon <165138530+bambxxz1e@users.noreply.github.com>
2025-03-12 11:18:54 +0900

    [level 0] Title: 대문자와 소문자, Time: 16.54 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -4,14 +4,10 @@ class Solution {
         char arr[] = my_string.toCharArray();
         
         for(int i = 0; i < arr.length; i++){
-            if(arr[i] <= 90) {
-                arr[i] += 32;
-                answer += String.valueOf(arr[i]);
-            }
-            else{
-                arr[i] -= 32;
-                answer += String.valueOf(arr[i]);
-            }
+            if(Character.isLowerCase(arr[i]))
+                answer += Character.toUpperCase(arr[i]);
+            else
+                answer += Character.toLowerCase(arr[i]);
         }
         return answer;
     }
