3d14b9a4b11015ebd4b685e7590d3a05932c7d49
yunseongyeon <165138530+bambxxz1e@users.noreply.github.com>
2025-03-12 11:29:18 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -2,9 +2,9 @@ class Solution {
     public String solution(String my_string, int num1, int num2) {
         String answer = "";
         char arr[] = my_string.toCharArray();
-        char a = arr[num2];
-        arr[num2] = arr[num1];
-        arr[num1] = a;
+        
+        arr[num1] = my_string.charAt(num2);
+        arr[num2] = my_string.charAt(num1);
         
         answer = String.valueOf(arr);
         
