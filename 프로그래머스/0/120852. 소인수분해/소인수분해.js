00924571355c4b5d73b5c3a09a9def83c17fe802
yunseongyeon <165138530+bambxxz1e@users.noreply.github.com>
2025-07-24 15:09:09 +0900

    [level 0] Title: 소인수분해, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -2,14 +2,11 @@ function solution(n) {
     var answer = [];
     
     let s = 2;
-    let pre = 0;
     
     while(n != 1){
         if(n % s == 0){
-            if(s != pre){
+            if(!answer.includes(s))
                 answer.push(s);
-                pre = s;
-            }
             n /= s;
         }
         else s++;
