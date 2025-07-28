5544da08f542ca6781b05fa1df40bcf9bdbe905b
yunseongyeon <165138530+bambxxz1e@users.noreply.github.com>
2025-07-28 21:56:41 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.64 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,27 +1,13 @@
 class Solution {
     public long solution(String numbers) {
-        long answer = 0;
-        String nums = "";
+        String numbers_arr[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
-        String n = "";
-        for(int i = 0; i < numbers.length(); i++){
-            n += numbers.charAt(i);
-            
-            switch(n){
-                case "zero" : nums += '0'; n = ""; break;
-                case "one" : nums += '1'; n = ""; break;
-                case "two" : nums += '2'; n = ""; break;
-                case "three" : nums += '3'; n = ""; break;
-                case "four" : nums += '4'; n = ""; break;
-                case "five" : nums += '5'; n = ""; break;
-                case "six" : nums += '6'; n = ""; break;
-                case "seven" : nums += '7'; n = ""; break;
-                case "eight" : nums += '8'; n = ""; break;
-                case "nine" : nums += '9'; n = ""; break;
-            }
+        for(int i = 0; i < numbers_arr.length; i++){
+            numbers = numbers.replaceAll(numbers_arr[i], String.valueOf(i));
+            // 숫자 단어를 배열 인덱스로
         }
         
-        answer = Long.parseLong(nums);
+        long answer = Long.parseLong(numbers);
         return answer;
     }
 }
