9efdbf9aac171f4c3dee7c6e8a8fe231162a8db8
yunseongyeon <165138530+bambxxz1e@users.noreply.github.com>
2025-11-03 13:53:09 +0900

    [level 0] Title: 커피 심부름, Time: 0.07 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -2,9 +2,9 @@ def solution(order):
     answer = 0
     
     for s in order:
-        if 'americano' in s or 'anything' in s:
-            answer += 4500
-        elif 'cafelatte' in s:
+        if 'cafelatte' in s:
             answer += 5000
+        else:
+            answer += 4500
     
     return answer
